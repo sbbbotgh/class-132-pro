d38675818701a703ea95d0d9ffd61843a4f8fb53
sbbbotgh <76098746+sbbbotgh@users.noreply.github.com>
2022-05-21 12:19:42 +0530

    Add files via upload

---
+++ pg3.js
@@ -0,0 +1,37 @@
+statuss = "";
+img = "";
+
+function preload(){
+    img = loadImage("images.jfif");
+}
+
+function setup(){
+    canvas = createCanvas(500, 300);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+    statuss = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log("error");
+    }
+    else{
+        console.log(results);
+    }
+}
+
+function draw(){
+    canvas.center();
+    image(img, 0, 0, 500, 300);
+}
+
+
+function back(){
+    window.location = "index.html";
+}
